81b6e64cc47ff1cc0225dc52aaabd8fdf2e34697
Michal Wachowski <michal.wachowski@olx.com>
2024-08-15 17:46:34 +0200

    PSQL persistence fails if stream was modified from outside

--- src/test/kotlin/potfur/es/PostgresPersistenceTest.kt
+++ src/test/kotlin/potfur/es/PostgresPersistenceTest.kt
@@ -1,14 +1,21 @@
 package potfur.es
 
 import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.flatMap
+import dev.forkhandles.result4k.orThrow
 import dev.forkhandles.result4k.peekFailure
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.BeforeClass
 import potfur.es.example.Transactor
+import strikt.api.expectThrows
 import java.util.UUID
 import kotlin.test.BeforeTest
+import kotlin.test.Test
 
 private fun Env(name: String, default: String = "") = System.getenv(name) ?: default
 
@@ -22,9 +29,11 @@ class ExposedTransactor(private val database: Database) : Transactor {
 }
 
 class PostgresPersistenceTest : PersistenceTestCase() {
+    private val serde = SimpleClassNameSerDe<Event>()
+
     override val streamId = UUID.randomUUID()
     override val transactor = ExposedTransactor(connection)
-    override val persistence = PostgresPersistence("event_store", SimpleClassNameSerDe<Event>())
+    override val persistence = PostgresPersistence("event_store", serde)
 
     companion object {
         lateinit var connection: Database
@@ -48,5 +57,33 @@ class PostgresPersistenceTest : PersistenceTestCase() {
             SchemaUtils.create(persistence)
         }
     }
+
+    @Test
+    fun `it raises exception when stream was modified during transaction`() {
+        transactor.primary {
+            persistence.store(streamId, 0, listOf(Event()))
+        }.orThrow()
+
+        expectThrows<ExposedSQLException> {
+            transactor.primary {
+                persistence.read(streamId)
+                    .flatMap {
+                        persistence.insertEventDirectly(streamId, it.size, Event())
+                        Success(it)
+                    }
+                    .flatMap { persistence.store(streamId, it.size, listOf(Event())) }
+            }
+        }
+    }
+
+    private fun PostgresPersistence.insertEventDirectly(streamId: UUID, offset: Int, event: Event) =
+        insert {
+            it[this.id] = event.id
+            it[this.streamId] = streamId
+            it[this.name] = event::class.simpleName ?: "unknown"
+            it[this.offset] = offset
+            it[this.payload] = serde.serialize(event)
+            it[this.timestamp] = event.timestamp
+        }
 }
 
